2e145557231d222615fc2ec876c522acda2c6741
Oğuz Kurt <kurt.oguz@outlook.com>
2022-06-28 00:24:58 +0300

    Drivetrain-related files created

---
+++ src/main/java/frc/robot/subsystems/Module.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+public class Module {
+    
+    private TalonFX m_driveMotor;
+    private TalonSRX m_turnMotor;
+
+}
